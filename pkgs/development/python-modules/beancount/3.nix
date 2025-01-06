f134311a0286ac47ffa5b200c3ba83a487ad207e
Andrei Lapshin <alapshin@fastmail.com>
2025-01-06 20:45:14 +0100

    python312Packages.beancount3: init at 3.0.0

--- pkgs/development/python-modules/beancount/3.nix
+++ pkgs/development/python-modules/beancount/3.nix
@@ -1,53 +1,56 @@
 {
   lib,
+  beautifulsoup4,
   buildPythonPackage,
+  click,
   fetchPypi,
   isPy3k,
-  beautifulsoup4,
-  bottle,
-  chardet,
+  meson,
+  meson-python,
+  pytestCheckHook,
   python-dateutil,
-  google-api-python-client,
-  google-auth-oauthlib,
-  lxml,
-  oauth2client,
-  ply,
-  pytest,
-  python-magic,
-  requests,
+  regex,
 }:
 
 buildPythonPackage rec {
-  version = "2.3.6";
-  format = "setuptools";
+  version = "3.0.0";
   pname = "beancount";
+  pyproject = true;
 
   disabled = !isPy3k;
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-gB+Tvta1fS4iQ2aIxInVob8fduIQ887RhoB1fmDTR1o=";
+    hash = "sha256-z2aGhpx+o+78CU7hPthmv196K7DGHk1PXfPjX4Rs/98=";
   };
 
   # Tests require files not included in the PyPI archive.
+  # Also there is an import error after migration to meson build.
   doCheck = false;
+
+  build-system = [
+    meson
+    meson-python
+  ];
+
+  depedencies = [
+    click
+    python-dateutil
+    regex
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
 
   propagatedBuildInputs = [
     beautifulsoup4
-    bottle
-    chardet
+    click
     python-dateutil
-    google-api-python-client
-    google-auth-oauthlib
-    lxml
-    oauth2client
-    ply
-    python-magic
-    requests
-    # pytest really is a runtime dependency
-    # https://github.com/beancount/beancount/blob/v2/setup.py#L81-L82
-    pytest
+    regex
   ];
+
+  pythonImportsCheck = [ "beancount" ];
 
   meta = {
     homepage = "https://github.com/beancount/beancount";
@@ -58,9 +61,6 @@ buildPythonPackage rec {
       generate a variety of reports from them, and provides a web interface.
     '';
     license = lib.licenses.gpl2Only;
-    maintainers = with lib.maintainers; [
-      sharzy
-      polarmutex
-    ];
+    maintainers = with lib.maintainers; [ bhipple ];
   };
 }
