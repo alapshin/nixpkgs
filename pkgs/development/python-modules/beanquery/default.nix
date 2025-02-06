3575f1a7af0a4161e524349f5bc08c9685f31ae1
Andrei Lapshin <alapshin@fastmail.com>
2025-02-06 12:22:08 +0100

    python313Packages.beanquery: 0.1.0 -> 0.2.0-unstable-2025-01-10

--- pkgs/development/python-modules/beanquery/default.nix
+++ pkgs/development/python-modules/beanquery/default.nix
@@ -9,16 +9,27 @@
   setuptools,
   tatsu,
 }:
+let
+  tatsu-lts = tatsu.overrideAttrs (oldAttrs: {
+    src = fetchFromGitHub {
+      owner = "dnicolodi";
+      repo = "TatSu-LTS";
+      version = "5.13.1";
+      tag = "v5.13.1-LTS";
+      hash = "sha256-cfGAWZDAnoD3ddhVIkOHyiv7gUDgnAWu1ZBvDEiQ2AQ=";
+    };
+  });
+in
 buildPythonPackage rec {
   pname = "beanquery";
-  version = "0.1.0";
+  version = "0.2.0-unstable-2025-01-10";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "beancount";
     repo = "beanquery";
-    tag = "v${version}";
-    hash = "sha256-1+KTUvnqPceRbzY1OZwOSQdK7f78K9kSwtQfI1SUIa8=";
+    rev = "e77a67996a54eef2e9d77b6352c74a40164e281d";
+    hash = "sha256-XYfKAscm55lY4YjIGTQ6RMFnCPWemfszpheGQ9qjMiM=";
   };
 
   build-system = [ setuptools ];
@@ -27,15 +38,12 @@ buildPythonPackage rec {
     beancount
     click
     python-dateutil
-    tatsu
+    tatsu-lts
   ];
-
-  pythonRelaxDeps = [ "tatsu" ];
 
   nativeCheckInputs = [ pytestCheckHook ];
 
   pythonImportsCheck = [
-    "beancount"
     "beanquery"
   ];
 
